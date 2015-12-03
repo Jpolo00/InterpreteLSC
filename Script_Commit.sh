b7a4bcba29069d700c2a04088716326c2680e8bf
Jpolo00 <jpolo00@hotmail.com>
2015-12-03 16:40:56 -0500

    Hola

--- Script_Commit.sh
+++ Script_Commit.sh
@@ -1,9 +1,8 @@
 echo "Script Commit, para facilitarnos la vida :3\n"
-echo "Creado por: Jp\n"
+echo "Creado por: Jp"
 echo "Mensaje del Commit: "
 read msgGit
 
 git add .
-git status
 git commit -m `echo "$msgGit"`
 git push origin master
