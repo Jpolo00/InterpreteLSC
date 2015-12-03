4f110cc75f3520a8e1c87cbff624c53487011288
Jpolo00 <jpoloasdasd>
2015-12-03 17:52:38 -0500

    Directorios

--- Script_Commit.sh
+++ Script_Commit.sh
@@ -10,7 +10,7 @@ echo -e "\e[1;32mListo para el Commit\e[0m"
 echo -e "Ingrese un mensaje para el commit:"
 read msgGit
 
-echo -e "\e[1;32mCreando Commit\e[0m"
+echo -e "\n\e[1;32mCreando Commit\e[0m"
 git commit -m `echo "$msgGit"`
 
 echo -e "Enter para \e[1;32mContinuar \e[0mo Ctrl+C para \e[1;31mCancelar\e[0m"
@@ -19,4 +19,4 @@ read enter
 echo -e "\e[1;32mHaciendo Push\e[0m"
 git push origin master
 
-echo -e "\e[1;31mFin Script :3"
+echo -e "\n\e[1;31mFin Script :3"
--- Script_Init.sh
+++ Script_Init.sh
@@ -1,13 +1,22 @@
-echo "Script Init, para facilitarnos la vida :3\n"
-echo "Creado por: Jp\n"
-echo "Solo ejecutarlo cuando se deba clonar el repositorio\n\n"
-echo "Ruta donde desea Clonar:" 
-read path
+#!/bin/bash
+echo -e "\n\n\e[1;32mScript Init, para facilitarnos la vida"
+echo -e "Creado por: Jp"
+echo -e "Solo ejecutarlo cuando se deba clonar el repositorio\e[0m"
+
 echo "Repositorio a Clonar <URL HTTPS>" 
 read repo
-
-cd $path
 git clone $repo
+
+echo -e "\e[1;32mRepositorio Clonado\e[0m"
+echo -e "Configurando GitHub"
+echo -e "<user> <email>"
+read user email
+
+git config --global --replace-all user.name "$user"
+git config --global --replace-all user.email "$email"
+
+echo -e "\e[1;31mFin Script"
+
 
 
 
