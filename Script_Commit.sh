dfb011b08339caf71bf00010ce4b876e96542c85
Jpolo00 <jpolo00@hotmail.com>
2015-12-03 17:04:39 -0500

    Prueba

--- Script_Commit.sh
+++ Script_Commit.sh
@@ -1,9 +1,14 @@
-echo "Script Commit, para facilitarnos la vida :3\n"
+#!/bin/bash
+echo "Script Commit, para facilitarnos la vida...\n"
 echo "Creado por: Jp"
-echo "Mensaje del Commit: "
-read msgGit
 
 git add .
 git status
+echo -e "\e[1;31mListo para el Commit\e[0m"
+echo -e "Ingrese un mensaje para el commit:"
+read msgGit
+echo -e "Enter para \e[1;32mContinuar \e[0mo Ctrl+C para \e[1;31mCancelar"
+read enter
 git commit -m `echo "$msgGit"`
 git push origin master
+echo -e "\e[1;31Fin Script :3"
