e5faa923f87c89bfa58ddba68a791eba5a9fa486
Jpolo00 <jpolo00@hotmail.com>
2015-12-13 00:53:05 -0500

    Diagramas Agregados

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+//int main(int argc, char const *argv[])
+int main()
 {
     //   +-----------------------------------------------+
     //   |Usa este main para todas las pruebas que decees|
