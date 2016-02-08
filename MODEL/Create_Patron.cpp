5127f25457c10730a098d0be3a6c7d93f75ff0e7
Jpolo00 <jpolo00@hotmail.com>
2016-02-08 18:02:15 -0500

    Empezando Create_Patron

--- MODEL/Create_Patron.cpp
+++ MODEL/Create_Patron.cpp
@@ -1,26 +1,4 @@
 #include "Create_Patron.h"
 
-// Create_Patron::Create_Patron(int numV, string letra)
-// 	this->numVectors=numVectors;
-// 	this->letter=letter;
-// }
-// Create_Patron::~Create_Patron(){}
-
-
-// void Create_Patron::addVectors(vector<double> patron)
-// {
-//    sinNombre.push_back(patron);
-// }
-
-// void Create_Patron::createPattern()
-// {
-// 	double temp=0.0;
-// 	for(int i=0; i<7; i++){
-// 		for(int j=0; j<sinNombre.size(); j++){
-// 			temp += sinNombre[j][i];
-// 		}
-// 		int aux= temp/sinNombre.size();
-// 		patron.push_back(aux);
-// 		temp=0.0;
-// 	}
-// }
+Create_Patron::Create_Patron() {}
+Create_Patron::~Create_Patron() {}
--- MODEL/Create_Patron.h
+++ MODEL/Create_Patron.h
@@ -12,16 +12,9 @@ using namespace std;
 class Create_Patron
 {
     private:
-        int numVectors;
-        string letter;
-        vector<vector<double> > sinNombre;
-        vector<double> patron;
 
     public:
-        Create_Patron(int numVectors, string letter);
+        Create_Patron();
         ~Create_Patron();
-
-        void addVectors(vector<double> patron);
-        void createPattern();
 };
 #endif
